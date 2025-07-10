8c5053a6acd6bd12e21b0c9bd2f571faf9d27d35
Ramnaresh Ahirwar <ramnareshahi77@gmail.com>
2025-07-10 18:02:07 +0000

    intermediate layer has been updated.

--- models/intermediate/int_quizzes.sql
+++ models/intermediate/int_quizzes.sql
@@ -13,12 +13,52 @@ WITH quizzes AS(
         JOIN
         {{ ref('stg_quizzes')}} q USING(answer_id)
 
+),
+
+attempts AS(
+    SELECT 
+        student_id,
+        COUNT(question_id) as total_attempts
+    FROM 
+        {{ ref('stg_student_quizzes')}}
+    GROUP BY
+        student_id
+),
+
+answers AS(
+    SELECT
+        s.student_id,
+        COUNT(q.answer_id) as total_correct_answers
+    FROM
+        {{ ref('stg_quizzes')}} q
+        JOIN
+        {{ ref('stg_student_quizzes')}} s USING(answer_id)
+    WHERE
+        q.answer_correct = 'y'
+    GROUP BY
+        s.student_id
+),
+
+final AS(
+    SELECT
+        q.quiz_id,
+        q.student_id,
+        q.question_id,
+        q.answer_correct,
+        att.total_attempts,
+        an.total_correct_answers,
+        CONCAT(an.total_correct_answers,'/',att.total_attempts) as result_overview
+    FROM
+        attempts att
+        JOIN
+        quizzes q USING(student_id)
+        JOIN
+        answers an USING(student_id)
 )
 
-SELECT 
-    quiz_id,
-    student_id,
-    question_id,
-    answer_correct
+SELECT
+    *
 FROM
-    quizzes
+    final
+ORDER BY
+    total_correct_answers DESC
